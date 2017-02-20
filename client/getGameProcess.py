3e5bf506f700c17dc0a9867e51d5dc96c9a25f5e
Inne <Inne-Lemstra@users.noreply.github.com>
2017-02-20 17:30:03 +0100

    is now loop and has a time out command input feature

--- client/getGameProcess.py
+++ client/getGameProcess.py
@@ -3,9 +3,11 @@ import re
 import json
 import imp
 import all_the_games
-from threading import Timer
+import time
+import msvcrt
+import sys
 
-def initiateGamesList(gamesListFilePath = "something"):
+def initiateGamesList():
 	imp.reload(all_the_games)
 	gameList = all_the_games.initiate() 
 	exeList = []
@@ -22,32 +24,44 @@ def initiateGamesList(gamesListFilePath = "something"):
 	exeDict = {i[0]:i[1]for i in exeList}
 	return exeDict
 
-def listenForCommands(timeOut = 60):
-	timeWindow = Timer(timeOut, print, ["Wait a minute, refreshing tasklist"])
-	command =  False
-	timeWindow.start()
-	command = input("Input command now: (type help to see options)\n")
-	timeWindow.cancel()
-	return command
+class TimeoutExpired(Exception):
+    pass
+
+def listenForCommands(prompt, timeout, timer=time.monotonic):
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    endtime = timer() + timeout
+    result = []
+    while timer() < endtime:
+        if msvcrt.kbhit():
+            result.append(msvcrt.getwche()) #XXX can it block on multibyte characters?
+            if result[-1] == '\r':   #XXX check what Windows returns here
+                return ''.join(result[:-1])
+        time.sleep(0.04) # just to yield to other processes/threads
+    return ""
 			
 if __name__ == "__main__":
-	fileToSend = open("info.txt","w")		
-	tasksByte = subprocess.check_output("tasklist")
-	tasks = tasksByte.decode("utf-8").split("\r\n")
-	#tasks_handle = open("example.txt", "r")
-	#tasks = tasks_handle.readlines()
+	command = ""
+	while not(command == "quit"):
+		fileToSend = open("info.txt","w")		
+		tasksByte = subprocess.check_output("tasklist")
+		tasks = tasksByte.decode("utf-8").split("\r\n")
+		#tasks_handle = open("example.txt", "r")
+		#tasks = tasks_handle.readlines()
 
+		exeList = initiateGamesList()
+		## get to the outpur line that is  only ===== === ==
+		tabGaps = [tabGap.start() for tabGap in re.finditer(" ",tasks[2])] 
+		#ending index of Name, IDm sessionName, Sessionnr, memoryUsage
 
-	## get to the outpur line that is  only ===== === ==
-	tabGaps = [tabGap.start() for tabGap in re.finditer(" ",tasks[2])] 
-	#ending index of Name, IDm sessionName, Sessionnr, memoryUsage
-
+		for process in tasks:
+			processName = process[:tabGaps[0]].strip() #remove whitspace at the end
+			gameName = exeList.get(processName, False)
+			if gameName:
+		#		fileToSend.write(gameName)
+				print("Now playing {0}".format(gameName))
 		
-
-	for process in tasks:
-		processName = process[:tabGaps[0]].strip() #remove whitspace at the end
-		gameName = exeList.get(processName, False)
-		if gameName:
-	#		fileToSend.write(gameName)
-			print("Now playing {0}".format(gameName))
-
+		print("not playing any games")
+		command = listenForCommands("type quit to cancel\n", 90)
+				
+				
