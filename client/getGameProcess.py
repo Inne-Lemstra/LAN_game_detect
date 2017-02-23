edfddb3d602a5b290c5480519ee4b74434b4c767
Inne <Inne-Lemstra@users.noreply.github.com>
2017-02-23 01:30:19 +0100

    halfway into adding add and help commands

--- client/getGameProcess.py
+++ client/getGameProcess.py
@@ -30,7 +30,7 @@ def initiateGamesList():
 			exeList = exeList + exesAndNames
 	
 	exeDict = {i[0]:i[1]for i in exeList}
-	return exeDict
+	return (exeDict, gameList)
 
 class TimeoutExpired(Exception):
     pass
@@ -43,13 +43,35 @@ def listenForCommands(prompt, timeout, timer=time.monotonic):
     while timer() < endtime:
         if msvcrt.kbhit():
             result.append(msvcrt.getwche()) #XXX can it block on multibyte characters?
-            if result[-1] == '\r':   #XXX check what Windows returns here
+            if result[-1] == '\r':   #XXX check what Windows returns here (it is \r)
                 return ''.join(result[:-1])
         time.sleep(0.04) # just to yield to other processes/threads
     return ""
-			
+
+def help():
+	"""Print documentation to screen, so user can see what all the availible commands do"""
+	print("this is going to be a help function")	
+
+def add(exeDict, gameListJson):
+	"""Add a exeName and GameName dictionary to all_the_games so program can recognize games not in the list"""
+	exeName = input("enter exe name or q to cancel")
+	if exeName == "q": break
+	gameName = input("enter game name (use UPPER case for every seperate word) or q to cancel")
+	if gameName == "q": break
+	errata = {"executables": {"win32": [exeName]},"id": gameListJson[-1]["id"] + 1, "name": gameName}
+	gameListJson.append(errata)
+	with open("all_the_games.txt", "r") as gameListHandle:
+		gameListHandle.write(json.dumps(gameListJson, indent = 4))
+	gameListHandle.close()
+	exeDict[exeName] = gameName
+	return (exeList, gameListJson)
+	
+	
+	
+
 if __name__ == "__main__":
 	command = ""
+	availibleCommands = ["add", "help"]
 	while not(command == "quit"):
 		fileToSend = open("info.txt","a")		
 		tasksByte = subprocess.check_output("tasklist")
@@ -57,10 +79,10 @@ if __name__ == "__main__":
 		#tasks_handle = open("example.txt", "r")
 		#tasks = tasks_handle.readlines()
 
-		exeList = initiateGamesList()
+		(exeList, gamesListJson) = initiateGamesList()
 		## get to the outpur line that is  only ===== === ==
 		tabGaps = [tabGap.start() for tabGap in re.finditer(" ",tasks[2])] 
-		#ending index of Name, IDm sessionName, Sessionnr, memoryUsage
+		#ending index of Name, ID, sessionName, Sessionnr, memoryUsage
 		currentlyPlaying = False
 		
 		for process in tasks:
@@ -73,6 +95,8 @@ if __name__ == "__main__":
 		
 		if not(currentlyPlaying): print("Not playing any games")
 		command = listenForCommands("type quit to stop program\n", 10)
+		if command in availibleCommands:
+			pass
 	fileToSend.close()
 	print("goodbye")
 				
