175327059e75f374550a36b3c45207bcd35666d3
Inne <Inne-Lemstra@users.noreply.github.com>
2017-02-23 16:18:34 +0100

    some changes that probably will form a conflict

--- server/testFlask.py
+++ server/testFlask.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
-	print(request.form['file'] + "\t from {0}".format(request.remote_addr))
+	print(request.form['file'])
 	with open('logLAN.txt', 'a') as logLANHandle:
 		logLANHandle.write("{0}\t{1}".format(request.remote_addr,request.form['file']))
 	logLANHandle.close()
